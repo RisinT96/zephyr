36edd034fe32afa49509e9cef8a6e5478998a164
Tal Risin <risint96@gmail.com>
2022-06-25 14:37:18 +0300

    driver: sensor: icm20648: add initial skeleton
    Added initial skeleton for implementing the icm20648 driver.
    
    Signed-off-by: Tal Risin <risint96@gmail.com>

---
+++ drivers/sensor/icm20648/icm20648.c
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2022 Tal Risin
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT invensense_icm20648
+
+#include "icm20648.h"
+
+LOG_MODULE_REGISTER(ICM20648, CONFIG_SENSOR_LOG_LEVEL);
---
+++ drivers/sensor/icm20648/icm20648.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2022 Tal Risin
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_SENSOR_ICM20648_ICM20648_H_
+#define ZEPHYR_DRIVERS_SENSOR_ICM20648_ICM20648_H_
+
+#endif /* __SENSOR_ICM20648 */
